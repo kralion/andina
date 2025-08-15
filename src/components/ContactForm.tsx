2f7499b30cdd290d0d65ae43bdcd69d3c7e7d6e1
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2025-08-15 11:59:35 -0500

    Sección Index terminada

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,7 +5,7 @@ import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@
 
 export default function ContactForm() {
   return (
-    <aside className="self-stretch md:block h-full px-4 md:px-0">
+    <aside className="self-stretch md:block h-full px-2 md:px-0">
       <div className="rounded-xl bg-white/10 p-6 backdrop-blur py-10">
         <h2 className="mb-4 text-center text-2xl font-semibold">
           Programa una consulta <br /> gratuita
@@ -26,7 +26,7 @@ export default function ContactForm() {
           </Select>
           <Textarea rows={4} className="rounded-md bg-white/90 px-3 py-2 text-black h-28" placeholder="Detalles" />
           <Button type="submit" className="mt-2 rounded-md bg-red-600 px-4 py-2 font-medium text-white hover:bg-red-700">
-            Reserve una consulta
+            Reserve una consulta gratuita
           </Button>
         </form>
       </div>
